89aac366375f7d5194c47ea80999794e0006a8aa
zer0 <zer0@hack.zer0.org>
2018-08-28 22:57:07 -0500

    version inicial del demo retrofit

---
+++ 2.3/retrofit-demo/src/main/java/org/zer0/pocs/retrofitdemo/RetrofitDemoApplication.java
@@ -0,0 +1,12 @@
+package org.zer0.pocs.retrofitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetrofitDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetrofitDemoApplication.class, args);
+	}
+}
